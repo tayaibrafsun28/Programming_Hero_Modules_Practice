d7ef76651f2291f365e9e593ae0788ac80fc4591
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-01-22 22:49:43 +0600

    Added 13-2 to 13-7 practice

---
+++ Milestone 3/Module 13/13-4.js
@@ -0,0 +1,8 @@
+var egg = 2;
+var apple = 20;
+var fish = 200;
+var meat = 2000;
+
+console.log(egg);
+console.log(egg + apple);
+console.log(egg + apple + fish);
---
+++ Milestone 3/Module 13/13-7.js
@@ -0,0 +1,4 @@
+var mycurrenthome = "Dhaka"
+var my_current_home = "Dhaka" // snake case
+var myCurrentHome = "Dhaka" //camel case
+var MyCurrentHome = "Dhaka" // Pascal case
