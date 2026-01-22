afaa005ec59c4d8fa1cf38307245ccbaefe3fe1b
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-01-22 23:11:33 +0600

    Added 13-8 practice

---
+++ Milestone 3/Module 13/13-8.js
@@ -0,0 +1,11 @@
+// Convert String into number
+
+var str_to_num = parseInt('20')
+var str_to_num_2 = parseFloat('0.1')
+var str_to_num_3 = parseFloat('0.2')
+console.log(typeof str_to_num)
+console.log(str_to_num_2)
+var total = str_to_num_2 + str_to_num_3
+console.log(total)
+console.log(total.toFixed(2)) // this defines how many digits will be shown after point
+
