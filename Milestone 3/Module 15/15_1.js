dd61076d527576ec7f4b96407af1d029b3170c15
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-01-27 00:07:05 +0600

    Added Module 15_1 and 15_2 Practice

--- Milestone 3/Module 15/15_1.js
+++ Milestone 3/Module 15/15_1.js
@@ -1,6 +1,7 @@
 const nums = [0,1 ,2, 3,4, 5];
 const names = ['Abul', 'Babul', 'Kabul'];
 
+// Printing elements of an array using for loop
 
 for(const i of nums){
     console.log(i);
---
+++ Milestone 3/Module 15/15_2.js
@@ -0,0 +1,6 @@
+let num = 0;
+
+while(num <25){
+    console.log('Looping - ',num);
+    num+=5;
+}
