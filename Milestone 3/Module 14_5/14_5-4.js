dfba2c47d6d389a7bf5e3522c642a5d99df57def
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-01-26 22:50:36 +0600

    Added Module 14_5-4 Practice

---
+++ Milestone 3/Module 14_5/14_5-4.js
@@ -0,0 +1,28 @@
+const nums = [0,1 ,2, 3,4, 5];
+console.log(nums);
+
+// Adding new a element in the last of an array - UNSHIFT
+
+nums.unshift(-1);
+console.log(nums);
+
+// Adding new a element in the last of an array - PUSH
+
+nums.push(6);
+console.log(nums);
+
+// Adding new elements in the last of an array - PUSH
+
+nums.push(7,8,9);
+console.log(nums);
+
+// Remove the last element from an array - POP
+
+nums.pop();
+console.log(nums);
+
+// Remove the first element from an array - SHIFT
+
+nums.shift();
+console.log(nums);
+
