7c499eff0b4bed846ebc49f3c68aaaad216a9224
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-01-27 23:44:46 +0600

    Added Module 16_2 and 16_3 Practice

---
+++ Milestone 3/Module 16/16_3.js
@@ -0,0 +1,35 @@
+const name1 = 'Tayaib';
+const name2 = 'Rafsun';
+
+// Adding 2 array -CONCAT
+
+const naam = name1.concat(' ').concat(name2)
+console.log(naam);
+
+
+// Slicing array - SLICE
+
+const part = name1.slice(1,4);
+console.log(part);
+
+
+// Split an Sentence - SPLIT
+
+const sentence = 'I am a good boy'; 
+console.log(sentence.split(" ")); // This splits the sentence where white space is found
+
+const friends = 'Tayaib,Shimanto,Ema,Junaid'; 
+console.log(friends.split(",")); // This splits the sentence where comma , is found
+
+//Join string elements of an array - JOIN
+
+const friends2 = ['Tayaib','Shimanto','Ema','Junaid']; 
+console.log(friends2.join(" - ")); // This joins the elements of the array with -
+console.log(friends2.join(" || ")); // This joins the elements of the array with ||
+
+
+// Find if an element is available in the string - INCLUDES
+
+const name3 = 'Neymar';
+console.log(name3.includes('y'));
+
