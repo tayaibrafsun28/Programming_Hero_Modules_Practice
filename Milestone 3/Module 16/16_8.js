2bd27cbdda37b61224df55cf904c2fa6ace21705
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-01-31 18:32:24 +0600

    Added Module 16_8 Practice

---
+++ Milestone 3/Module 16/16_8.js
@@ -0,0 +1,11 @@
+const bottle = {
+    brand: 'Puma',
+    price: 450,
+    color: 'White',
+    isNewArrival: true
+}
+
+const keys = Object.keys(bottle); //What are the keys of this object
+console.log(keys);
+const values = Object.values(bottle); //What are the values of this object
+console.log(values);
