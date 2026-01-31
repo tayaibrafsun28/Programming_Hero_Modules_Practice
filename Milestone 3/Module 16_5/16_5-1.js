ce4850a070ef0e9e1a74cb73411e97bc0362083b
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-01-31 21:28:58 +0600

    Added Module 16_5-1 Practice

---
+++ Milestone 3/Module 16_5/16_5-1.js
@@ -0,0 +1,15 @@
+// For loop technique
+
+const friends = ['Shima', 'Ema', 'Juna'];
+
+for (let i = 0; i<friends.length; i++){
+    console.log(i+' : '+friends[i]);
+}
+
+//While loop technique
+
+let n = 0;
+while(n<friends.length){
+    console.log(n+' : '+friends[n]);
+    n++;
+}
