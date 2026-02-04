b3a0fdf7f3f6e629bde624cc5f74acfe08f570e6
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-02-04 21:47:04 +0600

    Added Module 17_3 Practice

---
+++ Milestone 3/Module 17/17-3.js
@@ -0,0 +1,9 @@
+function square (number){
+     console.log('The given number is: ',number);
+     const sqr = number*number;
+     console.log('The square of the number is: ',sqr);
+     
+}
+
+square(4);
+square(7);
