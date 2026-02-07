da3965dfcee2ffac4829724b6897e0a6f140fd1e
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-02-07 19:57:40 +0600

    Updated  Module 18_5 Practice

--- Milestone 3/Module 18/18-5.js
+++ Milestone 3/Module 18/18-5.js
@@ -1,3 +1,13 @@
 function noDupli(arr){
+    const newArr = [];
+    for(let i=0; i<arr.length; i++){
+        if(newArr.includes(arr[i])===false){
+            newArr.push(arr[i]);
+        }
+    }
 
+    return newArr;
 }
+
+const arr = noDupli([2,5,5,3,2,8,7,8,2,1,0,1,6,5,6,9,4,9]);
+console.log(arr);
