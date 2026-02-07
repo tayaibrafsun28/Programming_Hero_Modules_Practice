216b945a62045147e24089dcf87cdedfc3c1cc7a
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-02-07 19:22:14 +0600

    Updated  Module 18_2 Practice

--- Milestone 3/Module 18/18-1.js
+++
@@ -1,8 +0,0 @@
-function inchToFeet(inch){
-    const rem = inch%12;
-    const feet = (inch-rem)/12;
-    return {rem, feet};
-}
-
-const height = inchToFeet(63);
-console.log('Hight : '+ height.feet +' Feet '+ height.rem +' Inch ');
--- Milestone 3/Module 18/18-2.js
+++ Milestone 3/Module 18/18-2.js
@@ -1,20 +1,8 @@
-function oddAvg(nums){
-    const oddArr = [];
-    let sum = 0;
-    for (let i = 0; i<nums.length; i++){
-        if(nums[i]%2!==0){
-            oddArr.push(nums[i]);
-        }
-    }
-
-    for (let i = 0; i<oddArr.length; i++){
-        sum = sum+oddArr[i];
-    }
-
-    const avgOdd = sum/oddArr.length;
-    return avgOdd;
+function inchToFeet(inch){
+    const rem = inch%12;
+    const feet = (inch-rem)/12;
+    return {rem, feet};
 }
 
-const nums = [42,13,58,65,81,96,7,105];
-const avg = oddAvg (nums);
-console.log(avg);
+const height = inchToFeet(63);
+console.log('Hight : '+ height.feet +' Feet '+ height.rem +' Inch ');
