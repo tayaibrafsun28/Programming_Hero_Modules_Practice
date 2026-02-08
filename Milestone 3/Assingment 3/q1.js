997683c84884ae809ec14ab8504c198b856bac98
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-02-08 15:31:01 +0600

    Addedd Assingment Qus 1

---
+++ Milestone 3/Assingment 3/q1.js
@@ -0,0 +1,17 @@
+// Problem - 01 
+
+function newPrice(currentPrice , discount ) {
+    if(typeof(currentPrice)=="number" && typeof(discount)=="number"){
+        let discountedPrice = currentPrice - (currentPrice*discount)/100;
+        return discountedPrice.toFixed(3);
+    }
+
+    else{
+        return 'Invalid';
+    }
+}
+
+const input = newPrice(1500, 20);
+console.log(input);
+
+
