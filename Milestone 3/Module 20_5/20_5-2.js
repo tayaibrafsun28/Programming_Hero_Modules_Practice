53f2346763a95e03da5d5150d0d61878ffb09c85
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-02-09 22:58:18 +0600

    Added Module 20_5-2 Practice

---
+++ Milestone 3/Module 20_5/20_5-2.js
@@ -0,0 +1,22 @@
+const phones = [
+    {id:1, brand: 'Xiaomi', price:21500},
+    {id:2, brand: 'Realme', price:14500},
+    {id:3, brand: 'Xiaomi', price:29499},
+    {id:4, brand: 'Samsung', price:35000},
+    {id:5, brand: 'iPhone', price:180500},
+];
+
+
+function matchPhones(phones, search){
+    const match = [];
+    for(const phone of phones){
+        if (phone.brand.includes(search)) {
+            match.push(phone);
+        }
+    }
+
+    return match;
+};
+
+const match = matchPhones(phones, 'Xiaomi');
+console.log(match)
