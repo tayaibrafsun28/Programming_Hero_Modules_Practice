c7fbbbca9d87e2841f5ca0488928801ed21fe41b
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-02-18 16:41:32 +0600

    Added Module 22-5 Practice

---
+++ Milestone 4/Module 22/Javascript/22-5.js
@@ -0,0 +1,6 @@
+const element = document.getElementById('p');
+const element2 = document.getElementById('h1');
+
+element.style.color = 'red';
+element2.style.backgroundColor = 'gold';
+element.classList.add('font-red');
