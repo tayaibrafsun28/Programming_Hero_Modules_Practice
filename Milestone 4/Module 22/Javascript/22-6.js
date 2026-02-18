f1cc9dded6e79dc81bc65335b4b6883565efdf1c
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-02-18 21:17:49 +0600

    Updated Module 22-6 Practice

--- Milestone 4/Module 22/Javascript/22-6.js
+++ Milestone 4/Module 22/Javascript/22-6.js
@@ -1,5 +1,9 @@
-const cls = document.querySelectorAll('player');
+const cls = document.getElementsByClassName('players');
 
 for(const pl of cls){
     console.log(cls);
+    pl.style.backgroundColor = 'gold';
+    pl.style.border = '3px solid red';
+    pl.style.padding = '50px';
+
 }
