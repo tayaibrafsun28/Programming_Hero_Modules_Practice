8256d69c997c3c793eb3de2a3730c4a28b8743c9
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-02-18 22:10:04 +0600

    Updated Module 22-6 Practice

--- Milestone 4/Module 22/Javascript/22-6.js
+++ Milestone 4/Module 22/Javascript/22-6.js
@@ -1,9 +1,23 @@
 const cls = document.getElementsByClassName('players');
 
+// Applying style using JS
+
 for(const pl of cls){
     console.log(cls);
     pl.style.backgroundColor = 'gold';
     pl.style.border = '3px solid red';
     pl.style.padding = '50px';
-
 }
+
+
+// Showing the Childs of a node in console
+const bdPlayers = document.getElementById('bd').childNodes;
+console.log(bdPlayers);
+
+
+// Adding a new element in HTML using JS
+const newChild = document.createElement('li');
+newChild.innerText = 'Pele'
+
+const interPlayer = document.getElementById('international');
+interPlayer.append(newChild);
