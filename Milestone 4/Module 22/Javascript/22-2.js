b887dd66c4134dd52641de5789dc00ca100c64bd
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-02-18 15:55:24 +0600

    Updated Module 22-2 Practice

--- Milestone 4/Module 22/Javascript/22-2.js
+++ Milestone 4/Module 22/Javascript/22-2.js
@@ -5,3 +5,16 @@ const car = {
         console.log('Engine status : ', status);
     }
 }
+
+
+
+const licollection = document.getElementsByTagName('li');
+console.log(licollection); 
+
+for(const li of licollection){
+    console.log(li.innerText);
+}
+
+const idh1 = document.getElementById('h1');
+console.log(idh1.innerText);
+idh1.innerText = "HELLO BAI";
