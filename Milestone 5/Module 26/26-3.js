c20c567c11648db49988002e157baa3983182456
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-03-06 00:21:49 +0600

    Added Module 26-3 practice

---
+++ Milestone 5/Module 26/26-3.js
@@ -0,0 +1,8 @@
+// Template string
+
+const name = 'Tayaib Rafsun';
+let age = 24;
+let year = '4th';
+
+const mySelf = `My name is ${name}, I'm ${age} years old, I'm a ${year} Student`
+console.log(mySelf);
