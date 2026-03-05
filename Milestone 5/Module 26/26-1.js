cfb4889eba10c4e69e361f965024c8c0a7f88f6c
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-03-05 22:12:42 +0600

    Added Module 26-1 practice

---
+++ Milestone 5/Module 26/26-1.js
@@ -0,0 +1,16 @@
+//Logics of const
+
+const name = 'Bangladesh';
+
+// const name = 'Pakistan' -> Cannot reassign the variable
+// name = 'Afganistan' -> Cannot update the value of variable
+
+const newName = 'Dhaka_'+name; //This is valid to use the variable in different ways
+
+//Logics of let
+
+let price = 120;
+price = 150; // In let, we can reuse, redeclare, reassign a variable
+
+console.log(price);
+
