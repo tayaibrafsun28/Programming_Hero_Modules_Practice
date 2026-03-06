8bf074f10fd89214f7575630daae0842831a0c03
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-03-06 17:28:00 +0600

    Added Module 26-7 practice

---
+++ Milestone 5/Module 26/26-7.js
@@ -0,0 +1,13 @@
+const student = {name: 'Tayaib', id: 22201064, dept: 'CSE'};
+
+delete student.dept;
+
+Object.seal(student); // Allow modification but no add or delete
+
+student.name = 'Tayaib Rafsun';
+
+Object.freeze(student);  // Doesn't allow modification, add or delete
+
+delete student.id;
+
+console.log(student);
