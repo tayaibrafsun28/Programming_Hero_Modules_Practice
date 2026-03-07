11c3b49902324f2c8c11e92b2f1a511d3b047e9a
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-03-07 14:39:38 +0600

    Added Module 28-2 practice

---
+++ Milestone 5/Module 28/28-2.js
@@ -0,0 +1,17 @@
+const student = {
+    name: 'Tayaib', 
+    id: 22201064, 
+    dept: 'CSE'
+};
+
+console.log('Student: ',student, typeof student);
+
+const strStudent = JSON.stringify(student);
+console.log('strStudent: ',strStudent, typeof strStudent);
+
+const objStudent = JSON.parse(strStudent);
+console.log('objStudent: ', objStudent, typeof objStudent);
+
+
+
+
