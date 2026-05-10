344c80a392b632d3e32abb14591b99fa891dab08
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-05-10 21:47:14 +0600

    Added 22-7.js

---
+++ Milestone 4/Module 22/Javascript/22-7.js
@@ -0,0 +1,13 @@
+// Create element
+const newChild = document.createElement('li');
+
+
+// Add value
+newChild.innerText = 'Watermelon';
+
+// Get parent
+const parent = document.getElementById('fruits');
+
+// Append child
+
+parent.appendChild(newChild);
