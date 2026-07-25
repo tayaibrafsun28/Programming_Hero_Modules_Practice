58c30716435e001e00aa50d697eebfeb169a2794
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-07-25 22:49:16 +0600

    Added module 26-8.js

---
+++ Milestone 5/Module 26/26-8.js
@@ -0,0 +1,23 @@
+// Dot notation
+
+const player1 = {
+    name: 'Neymar',
+    age: 34,
+    number: 10,
+    country: 'Brazil',
+    club: 'Santos'
+}
+
+console.log(player1.name)
+
+// Bracket notation
+
+const player2 = {
+    name: 'Mbappe',
+    age: 27,
+    number: 10,
+    country: 'France',
+    club: 'Real Madrid'
+}
+
+console.log(player2['club'])
