90cf84f3d3130d18a404094cc95b3f462e556308
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-07-26 21:01:11 +0600

    Updated module 27-9.js

--- Milestone 5/module 27/27-9.js
+++ Milestone 5/module 27/27-9.js
@@ -1,4 +1,9 @@
 // Filter in js
-const friends = ['Adib','Shimanto', 'Ema', 'Junaid'];
-const first = friends.filter( frnds => frnds.length<4 || frnds.includes('Sh'));
-console.log(first);
+const friends1 = ['Adib','Shimanto', 'Ema', 'Junaid'];
+const friend = friends1.filter( frnds => frnds.length<4 || frnds.includes('Sh'));
+console.log(friend);
+
+// Find in js - gives just 1st output matched with 
+const friends2 = ['Adib','Abir', 'Abrar', 'Azmi'];
+const frnd = friends2.find( frnds => frnds.includes('A'));
+console.log(frnd);
