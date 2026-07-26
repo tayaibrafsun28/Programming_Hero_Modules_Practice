cd71de0d9604d4094efd7cda04de91942d9988f0
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-07-26 18:39:31 +0600

    updated module 27-5.js

--- Milestone 5/module 27/27-5.js
+++ Milestone 5/module 27/27-5.js
@@ -2,7 +2,7 @@
 
 function outer() {
     function inner() {
-        console.log('Hello Bai');        
+        console.log('Hello Bai \n');        
     }
     
     return inner;
@@ -10,3 +10,31 @@ function outer() {
 
 const calling = outer();
 calling();
+
+// Closure function counter example
+
+const counter = (owner) => {
+    let count = 0;
+
+    const increment = () =>{
+        count = count + 1;
+        console.log('Value of counter for : ', owner, count);
+    }
+
+    return increment;
+}
+
+const rahimCounter = counter('Rahim')
+const karimCounter = counter('Karim')
+
+rahimCounter()
+rahimCounter()
+rahimCounter()
+rahimCounter()
+
+karimCounter()
+karimCounter()
+karimCounter()
+
+rahimCounter()
+rahimCounter()
