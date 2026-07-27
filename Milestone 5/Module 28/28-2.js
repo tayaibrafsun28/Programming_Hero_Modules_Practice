f4208c58931b0aae14d17d145a10ca02e8888108
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-07-27 21:12:31 +0600

    Updated module 28-2.js

--- Milestone 5/Module 28/28-2.js
+++ Milestone 5/Module 28/28-2.js
@@ -5,10 +5,16 @@ const student = {
     graduate: false,
 };
 
+// Normal object
+
 console.log('Student: ',student, typeof student);
+
+// Transfer object to JSON string
 
 const strStudent = JSON.stringify(student);
 console.log('strStudent: ',strStudent, typeof strStudent);
+
+// Transfer JSON string to object
 
 const objStudent = JSON.parse(strStudent);
 console.log('objStudent: ', objStudent, typeof objStudent);
