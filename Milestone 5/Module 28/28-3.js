dd728a80d7102b2fea6fc43e5701345de40525c8
tayaibrafsun28 <tayaibrafsun28@gmail.com>
2026-07-28 21:48:42 +0600

    Updated module 28-3.js

--- Milestone 5/Module 28/28-3.js
+++ Milestone 5/Module 28/28-3.js
@@ -2,6 +2,6 @@
  // .then gives us a response but not the exact data we want
 // next .then gives us the data we want
 
-const result = fetch("https://jsonplaceholder.typicode.com/todos/1").then((response) => response.json()).then((data) => console.log(data))
+const result = fetch("https://jsonplaceholder.typicode.com/todos/1").then(response => response.json()).then(data => console.log(data))
 
 
